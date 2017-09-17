9d5dcb89ddbf69b7bd69b7df2195c455035c3804
Michael Lee <michaelleestuy@gmail.com>
2017-09-17 14:37:58 -0400

    problems one and two

--- 1.c
+++ 1.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 
 int main(){
-  int count = 0;
+  printf("Problem one: %d\n", problemone);
+
+  return 0;
+}
+
+int problemone(){
+  int sum = 0;
   int i;
   for(i = 0; i < 1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      count += i;
+    if(i%3 == 0 || i%5 == 0)
+      sum += i;
+  }
+  return sum; 
+}
+
+
+int problemtwo(){
+  int first = 1;
+  int second = 1;
+  int sum = 0;
+
+  int temp = 0;
+
+  while(second < 4000000){
+    if(second%2 == 0){
+      sum += second;
     }
+    temp = first + second;
+    first = second;
+    second = temp;
   }
 
-  printf("%d\n", count);
-  return 0;
+  return sum;
 }
